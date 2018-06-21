24e264ad09d7ce4c546988e72e3ada1f193be964
Gurvir Cheema <gurvircheema@hotmail.com>
2018-06-21 00:34:25 -0700

    Add upvote and downvote counters

--- app/models/feature_request.rb
+++ app/models/feature_request.rb
@@ -1,4 +1,12 @@
 class FeatureRequest < ApplicationRecord
+  validates :name, presence: true
+  belongs_to :created_by_user, class_name: 'User'
+  has_many :requests
 
-  validates :created_by_user_id, presence: true
+  def reset_counter
+    update(
+      upvotes: requests.upvote.count,
+      downvotes: requests.downvotes.count
+    )
+  end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,28 @@
 class Request < ApplicationRecord
+  belongs_to :user
+  belongs_to :feature_request
+  scope :upvotes, -> { where(upvote: true) }
+  scope :downvote, -> { where(upvote: false) }
+
+  validates :user_id, uniqueness: { scope: :feature_request_id }
+
+  after_create do
+    FeatureRequest.increment_counter(feature_request_column, feature_request.id)
+  end
+
+  after_update do
+    return unless saved_change_to_attribute?(:upvote)
+    FeatureRequest.increment_counter(feature_request_column, feature_request.id)
+    FeatureRequest.decrement_counter(inverse_feature_request_column, feature_request.id)
+  end
+
+  private
+
+  def feature_request_column
+    upvote? ? :upvotes : :downvotes
+  end
+
+  def inverse_feature_request_column
+    upvote? ? :downvotes : :upvotes
+  end
 end
