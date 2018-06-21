9cd098a44c7f96ae67fc5585fa298169ef864453
Gurvir Cheema <gurvircheema@hotmail.com>
2018-06-21 12:07:12 -0700

    Add react application

---
+++ app/javascript/packs/feature_request_voting.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class FeatureRequestVoting extends React.Component {
+  render() {
+    return <div>React application takes over</div>;
+  }
+}
+
+ReactDOM.render(
+  <FeatureRequestVoting />,
+  document.getElementById("feature-requests-app")
+);
--- app/views/feature_requests/_feature_request.json.jbuilder
+++ app/views/feature_requests/_feature_request.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! feature_request, :id, :name, :created_by_user_id, :created_at, :updated_at
+json.extract! feature_request, :id, :name, :created_by_user_id, :created_at, :updated_at, :difficulty, :details, :upvotes, :downvotes
 json.url feature_request_url(feature_request, format: :json)
